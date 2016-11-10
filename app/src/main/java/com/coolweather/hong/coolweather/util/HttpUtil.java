3098146e98c5326f5c26d003215d5a4ae16cdbc3
Hong <1160320185@qq.com>
2016-11-11 01:17:44 +0800

    修改了IO流抛出EOF异常的bug

--- app/src/main/java/com/coolweather/hong/coolweather/util/HttpUtil.java
+++ app/src/main/java/com/coolweather/hong/coolweather/util/HttpUtil.java
@@ -1,5 +1,7 @@
 package com.coolweather.hong.coolweather.util;
 
+import android.util.Log;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
@@ -14,7 +16,7 @@ import java.net.URL;
 
 public class HttpUtil {
 
-    public static void sendHttpRequest(final String address, final HttpCallBackListener listener){
+    public static void sendHttpRequest(final String address, final HttpCallBackListener listener) {
         new Thread(new Runnable() {
             @Override
             public void run() {
@@ -26,21 +28,31 @@ public class HttpUtil {
                     connection.setRequestMethod("GET");
                     connection.setConnectTimeout(8000);
                     connection.setReadTimeout(8000);
-                    in = connection.getInputStream();
-                    BufferedReader reader = new BufferedReader(new InputStreamReader(in));
-                    StringBuilder response = new StringBuilder();
-                    String line;
-                    while ((line=reader.readLine())!=null){
-                        response.append(line);
-                    }
-                    if (listener!=null){
-                        listener.onFinish(response.toString());
+                    if (connection.getResponseCode() == 200) {
+                        in = connection.getInputStream();
+//                        BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+                        StringBuffer response = new StringBuffer();
+//                        String line;
+//                        reader.read()
+//                        while ((line = reader.readLine()) != null) {
+//                            Log.i("line",line);
+//                            response.append(line);
+//                        }
+                        byte [] data = new byte[1024];
+                        int num = 0;
+                        if((num=in.read(data))!=0){
+                            String line = new String(data,0,num);
+                            response.append(line);
+                        }
+                        if (listener != null) {
+                            listener.onFinish(response.toString());
+                        }
                     }
                 } catch (Exception e) {
                     e.printStackTrace();
                     listener.onError(e);
-                }finally {
-                    if (connection!=null){
+                } finally {
+                    if (connection != null) {
                         connection.disconnect();
                     }
                 }
